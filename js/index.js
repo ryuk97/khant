8b22472588236726f1c0bbd961337f28b9a3441e
ryuk97 <kaungkhantnaing649@gmail.com>
2022-03-14 15:37:18 +0630

    first init

---
+++ js/index.js
@@ -0,0 +1,17 @@
+const up = document.querySelector(".up");
+const navbar = document.querySelector(".navbar");
+up.onclick = () => {
+    window.scrollTo(0, 0);
+}
+window.onscroll = () => {
+    if (window.scrollY >= 300) {
+        navbar.classList.add("navbar-bg");
+    } else {
+        navbar.classList.remove("navbar-bg");
+    }
+    if (window.scrollY >= 500) {
+        up.style.display = "block";
+    } else {
+        up.style.display = "none";
+    }
+}
